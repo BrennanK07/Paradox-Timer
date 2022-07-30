9abd4ecbea348260b7f02a3809847b8aa3926815
brennanrules124 <brennanrules124@gmail.com>
2022-07-30 17:33:24 -0400

    Session Creator Partially done

--- main.js
+++ main.js
@@ -6,71 +6,73 @@ let isPressing = false; //If the user is interacting (Space / touch / mouse)
 let pressTime; //Time the player press the start button
 let justFinished = false;
 
+let OnMainPage = true;
+
 //The main repeating function that determines all other actions for the website
-var mainFunction = window.setInterval(function(){
+var mainFunction = window.setInterval(function () {
+    if (OnMainPage) {
+        //Starts the timerQuery / timer if possible
+        if (isPressing == true && isSolving == false && waitingToStart == false && justFinished == false) {
+            ChangeTimerColor("#FF2222");
+            waitingToStart = true;
+            pressTime = new Date().getTime() / 1000;
 
-    //Starts the timerQuery / timer if possible
-    if(isPressing == true && isSolving == false && waitingToStart == false && justFinished == false){
-        ChangeTimerColor("#FF2222");
-        waitingToStart = true;
-        pressTime = new Date().getTime() / 1000;
+            //console.log("Started Query");
+        }
 
-        //console.log("Started Query");
-    }
+        //Below runs when the timer is in the "red" state
+        if (waitingToStart == true) {
+            if ((new Date().getTime() / 1000 - pressTime) >= 0.5) {
+                ChangeTimerColor("#22FF22");
+                //console.log("Waiting to start");
+            }
+        }
 
-    //Below runs when the timer is in the "red" state
-    if(waitingToStart == true){
-        if((new Date().getTime() / 1000 - pressTime) >= 0.5){
-            ChangeTimerColor("#22FF22");
-            //console.log("Waiting to start");
+        //Starts timer
+        if (waitingToStart == true && isPressing == false) {
+            waitingToStart = false;
+            isSolving = true;
+
+            StartTimer();
+            //console.log("Timer Started");
+        }
+
+        //Stops timer
+        if (isSolving == true && isPressing == true) {
+            isSolving = false;
+            justFinished = true;
+
+            StopTimer();
+            //console.log("Timer Stopping");
+        }
+
+        if (isSolving == true) {
+            ChangeTimerColor("#FFFFFF");
         }
     }
-
-    //Starts timer
-    if(waitingToStart == true && isPressing == false){
-        waitingToStart = false;
-        isSolving = true;
-
-        StartTimer();
-        //console.log("Timer Started");
-    }
-
-    //Stops timer
-    if(isSolving == true && isPressing == true){
-        isSolving = false;
-        justFinished = true;
-
-        StopTimer();
-        //console.log("Timer Stopping");
-    }
-
-    if(isSolving == true){
-        ChangeTimerColor("#FFFFFF");
-    }
-
 }, 10);
 
-function ChangeTimerColor(colorToChangeTo){
+function ChangeTimerColor(colorToChangeTo) {
     document.getElementById("timer").style.color = colorToChangeTo.toString();
 }
 
 
 
 //Listens for keyboard actions
-document.addEventListener('keydown', function(event) {
-    if(event.code == 'Space'){
+document.addEventListener('keydown', function (event) {
+    if (event.code == 'Space') {
         isPressing = true;
     }
 });
 
-document.addEventListener('keyup', function(event) {
-    if(event.code == 'Space'){
+document.addEventListener('keyup', function (event) {
+    if (event.code == 'Space') {
         isPressing = false;
         justFinished = false;
         ChangeTimerColor("#FFFFFF");
 
         //Resets the query if the timer failed to stat
-        if(waitingToStart == true && !((new Date().getTime() / 1000 - pressTime) >= 0.5)){
+        if (waitingToStart == true && !((new Date().getTime() / 1000 - pressTime) >= 0.5)) {
             waitingToStart = false;
         }
     }
--- session.js
+++ session.js
@@ -1,3 +1,14 @@
-function CreateNewSession(){
-  
+function CreateNewSession(cubeType, name) {
+
+}
+
+function OpenSessionGenerator() {
+  document.getElementById("sessionMenu").style.width = "100%";
+  OnMainPage = false;
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function CloseSessionGenerator() {
+  document.getElementById("sessionMenu").style.width = "0%";
+  OnMainPage = true;
 }
