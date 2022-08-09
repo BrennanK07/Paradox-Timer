98078e20c3ef95fa27f1212b88b4f61006eaad86
brennanrules124 <70231079+brennanrules124@users.noreply.github.com>
2022-08-08 22:45:34 -0400

    Just a beta build
    Just a beta build

--- focus.js
+++ focus.js
@@ -6,9 +6,11 @@ var testFunc = window.setInterval(function () {
 		console.log("Element Is not focused ")
 		document.getElementById("timer").style.filter = "blur(8px)";
 		IsFocused = false;
+		document.getElementById("InfoBox").innerHTML = "Click Here To Refocus";
 	} else {
 		console.log("Element is focused");
 		document.getElementById("timer").style.filter = "none";
 		IsFocused = true;
+		document.getElementById("InfoBox").innerHTML = "";
 	}
 }, 10);
