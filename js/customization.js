9826342676f7b49a8a306e5ff358de701a1e9c3c
brennanrules124 <70231079+brennanrules124@users.noreply.github.com>
2022-08-27 09:21:49 -0400

    Imrpoved UI

--- js/customization.js
+++ js/customization.js
@@ -4,13 +4,14 @@ var leftSideBarBackground = document.getElementById("Lsidenav")
 var rightSideBarBackground = document.getElementById("Rsidenav")
 var scrambleText = document.getElementById("ScrambleText")
 var scrambleBackground = document.getElementById("header")
-var titleTextColor = document.getElementById("title")
-var titleStripeColor = document.getElementById("title")
+var titleTextColor = document.querySelectorAll('.title')
+var titleStripeColor = document.querySelectorAll('.title')
 var buttonBackgroundColor = document.getElementById("button")
 var buttonBackgroundHover = document.getElementById("button:hover")
 var buttonTextColor = document.getElementById("button")
 var tableColor = document.getElementById("timeTable")
 var tableTextColor = document.getElementById("tableBody")
+var statColor = document.querySelectorAll('.Stat')
 
 var setConfig;
 
@@ -29,20 +30,66 @@ var DefaultConfig = {
     buttonBackgroundHover: "#717171",
     buttonTextColor: "#FFFFFF",
     tableColor: "#FFFFFF",
-    tableTextColor: "#00FF00"
+    tableTextColor: "#00FF00",
+    statColor: "#FFFFFF"
 };
+
+var DebugConfig = {
+    name: "Debug",
+    timerColor: "#000000",
+    background: "#000000",
+    leftSideBarBackground: "#000000",
+    rightSideBarBackground: "#000000",
+    scrambleText: "#000000",
+    scrambleBackground: "#000000",
+    titleTextColor: "#000000",
+    titleStripeColor: "#000000",
+    buttonBackgroundColor: "#000000",
+    buttonBackgroundHover: "#000000",
+    buttonTextColor: "#000000",
+    tableColor: "#000000",
+    tableTextColor: "#000000",
+    statColor: "#FFFFFF"
+}
 
 var Configs = [];
 Configs[0] = DefaultConfig;
+
+//DEBUG
+Configs[1] = DebugConfig;
 
 setConfig = GetActiveConfig();
 SetCustomizations(setConfig);
 
 function GetActiveConfig(){
-    //DEBUG, FIX LATER
+    //DEBUG, FIX LATER TO GRAB THE SET CONFIG
     return 0;
 }
 
 function SetCustomizations(layout) {
+    timer.style.color = Configs[layout].timerColor;
+    background.style.backgroundColor = Configs[layout].background;
+    leftSideBarBackground.style.backgroundColor = Configs[layout].leftSideBarBackground;
+    rightSideBarBackground.style.backgroundColor = Configs[layout].rightSideBarBackground;
+    scrambleText.style.color = Configs[layout].scrambleText;
+    scrambleBackground.style.backgroundColor = Configs[layout].scrambleBackground;
+
+    titleTextColor.forEach(title => {
+        title.style.color = Configs[layout].titleTextColor;
+    });
+
+    titleStripeColor.forEach(stripe => {
+        stripe.style.borderTop = "solid " + Configs[layout].titleStripeColor;
+        stripe.style.borderBottom = "solid " + Configs[layout].titleStripeColor;
+    });
     
+    buttonBackgroundColor = document.getElementById("button")
+    buttonBackgroundHover = document.getElementById("button:hover")
+    buttonTextColor = document.getElementById("button")
+    tableColor = document.getElementById("timeTable")
+    tableTextColor = document.getElementById("tableBody")
+
+    statColor.forEach(stat => {
+        stat.style.colorr = Configs[layout].titleTextColor;
+    });
 }
