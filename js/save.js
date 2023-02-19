9d0b106f00c036af1afa8a529c9c76787282f6f7
brennanrules124 <70231079+brennanrules124@users.noreply.github.com>
2023-02-19 16:56:50 -0500

    v1.10 - Bug Fixes

--- js/save.js
+++ js/save.js
@@ -14,16 +14,6 @@ function SaveData(){
     //console.log(JSON.parse(localStorage.getItem("SessionArray")));
 
     localStorage.setItem("Test", 1); //Value indicates if a save has happened already, and will make the load system attempt to load
-
-    //For backups
-    localStorage.setItem("SessionArrayBkp", JSON.stringify(SessionData));
-
-    localStorage.setItem("configurationInUseBkp", setConfig);
-
-    InitializeDefaultConfigs();
-    localStorage.setItem("ConfigsArrayBkp", JSON.stringify(ConfigData));
-
-    localStorage.setItem("Bkp", 1);
 }
 
 window.addEventListener("beforeunload", function(){
@@ -60,28 +50,12 @@ function LoadData() {
         }
 
         InitializeDefaultConfigs();
-    /*}else if(localStorage.getItem("Bkp") == 1){
+    }else{
         //console.log("No Save Data Found");
-
-        sessions = JSON.parse(localStorage.getItem("SessionArrayBkp"));
-
-        //console.log(JSON.parse(localStorage.getItem("ConfigsArray")));
-        Configs = JSON.parse(localStorage.getItem("ConfigsArrayBkp")); //Function re-runs as the configuration settings are initialized
-
-        for(var i = 0; i < sessions.length; i++){
-            AddSessionToList(sessions[i]);
-        }
-
-        setConfig = localStorage.getItem("configurationInUseBkp");
-        configurationMenu.selectedIndex = setConfig;
-        if(configurationMenu.selectedIndex == -1){
-            configurationMenu.selectedIndex = 0;
-        }
-
         if (typeof (Storage) !== "undefined") {
             // Code for localStorage/sessionStorage.
         } else {
             alert("WARNING! Your browser does not support Web Storage! Please use another browser to ensure that your data is saved.");
-        }*/
+        }
     }
 }
